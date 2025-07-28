3294ac0d6bc7251ffcfc1a2da82ed0e2474de46b
Ezequiel <equi32@gmail.com>
2025-07-28 20:28:26 -0300

    feat: Added RansomNote solution

---
+++ src/basic/RansomNote.java
@@ -0,0 +1,33 @@
+package basic;
+
+import java.util.HashMap;
+
+public class RansomNote {
+
+    public static boolean canConstruct(String ransomNote, String magazine) {
+        HashMap<Character, Integer> magazineLetters = new HashMap<>();
+
+        for (int i = 0; i < magazine.length(); i++) {
+            char m = magazine.charAt(i);
+            int currentCount = magazineLetters.getOrDefault(m, 0);
+            magazineLetters.put(m, currentCount + 1);
+        }
+
+        for (int i = 0; i < ransomNote.length(); i++) {
+            char r = ransomNote.charAt(i);
+
+            int currentCount = magazineLetters.getOrDefault(r, 0);
+            if (currentCount == 0) return false;
+
+            magazineLetters.put(r, currentCount - 1);
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("a -> b = " + canConstruct("a", "b"));
+        System.out.println("aa -> ab = " + canConstruct("aa", "ab"));
+        System.out.println("aa -> aab = " + canConstruct("aa", "aab"));
+    }
+}
