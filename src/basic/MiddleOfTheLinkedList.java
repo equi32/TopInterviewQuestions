1fc4ca82350d5e6ba3185660026b884e1c498725
Ezequiel <equi32@gmail.com>
2025-07-28 20:12:07 -0300

    feat: Added MiddleOfTheLinkedList solution

---
+++ src/basic/MiddleOfTheLinkedList.java
@@ -0,0 +1,33 @@
+package basic;
+
+public class MiddleOfTheLinkedList {
+    public static class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {}
+        ListNode(int val) { this.val = val; }
+        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    }
+
+    public static ListNode middleNode(ListNode head) {
+        ListNode middle = head;
+        ListNode end = head;
+
+        while (end != null && end.next != null) {
+            middle = middle.next;
+            end = end.next.next;
+        }
+
+        return middle;
+    }
+
+    public static void main(String[] args) {
+        ListNode fifth = new ListNode(5);
+        ListNode fourth = new ListNode(4, fifth);
+        ListNode third = new ListNode(3, fourth);
+        ListNode second = new ListNode(2, third);
+        ListNode first = new ListNode(1, second);
+
+        System.out.println(middleNode(first).val);
+    }
+}
