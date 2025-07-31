31317a3f39786bce296fb982170518046fde3a52
Ezequiel <equi32@gmail.com>
2025-07-31 17:31:15 -0300

    feat: Added PalindromeNumber solution

---
+++ src/basic/PalindromeNumber.java
@@ -0,0 +1,22 @@
+package basic;
+
+import java.util.Stack;
+
+public class PalindromeNumber {
+
+    public static boolean isPalindrome(int x) {
+        String value = String.valueOf(x);
+        StringBuilder sb = new StringBuilder(value);
+        try {
+            int reversedValue = Integer.parseInt(sb.reverse().toString());
+            return x == reversedValue;
+        } catch (Exception ex) {
+            return false;
+        }
+    }
+
+    public static void main(String[] args) {
+        var result = isPalindrome(-121) ? "Is Palindrome" : "Is Not Palindrome";
+        System.out.println(result);
+    }
+}
